39f804511d3ac41a5294257dae90d76128951335
Tiago Almeida <tiagoalmeida.dsv@gmail.com>
2025-07-15 10:22:38 -0300

    ajuste no script 05

--- 05 - Consulta de quantitativos.sql
+++ 05 - Consulta de quantitativos.sql
@@ -1,21 +1,21 @@
 
 select COUNT(cod_cli)
 from TbelCnt
-where tipo_modulo = 'CONTA_CORRENTE'
-      and stt_reg = 'ATIVO'
+where tipo_modulo = 'MODULO 01'
+      and stt_reg = 'LIBERADO'
       and dt_ult_mov > '2000-04-00'
       and cod_cli in (select F.cod_cli from TbelPessF F where dt_nas between '1958-10-01' and '2008-10-01')
 
 select COUNT(cod_cli)
 from TbelCnt
-where tipo_modulo = 'CONTA_CORRENTE'
-      and stt_reg = 'ATIVO'
+where tipo_modulo = 'MODULO 01'
+      and stt_reg = 'LIBERADO'
       and dt_ult_mov > '2020-00-00'
       and cod_cli in (select cod_cli from TbelPessF where dt_nas between '1957-10-01' and '1952-10-01')
 
 select COUNT(cod_cli)
 from TbelCnt
-where tipo_modulo = 'CONTA_CORRENTE'
-      and stt_reg = 'ATIVO'
+where tipo_modulo = 'MODULO 01'
+      and stt_reg = 'LIBERADO'
       and dt_ult_mov > '2000-00-00'
       and cod_cli = (select cod_cli from TbelPessF where dt_nas between '1951-10-01' and '1947-10-01')
